ea8cfb05b6e966c58217fbb62a67a6290174ee93
Рома Юсупов <roma_yusupov_05@mail.ru>
2022-09-22 07:47:04 +0300

    test edit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,50 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            //задание 1
+            //for (int i = 0; i <= 15; i +=1)
+            //{
+            //    Console.WriteLine(i);   
+            //}
+            //Console.ReadKey();
+
+            //задание 2
+            //char f = 'о';
+            //for(int i = 0; i <= 6; i++)
+            //{
+            //    Console.Write(f);
+            //    f++;
+            //}
+            //Console.ReadKey();
+
+            //задание 3
+            //for (var i = 0; i < 10; i++)
+            //{
+            //    for (var j = 0; j < 5; j++)
+            //    {
+            //        Console.Write('#');
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+
+            //задание 4
+            //for(int i = 0; i <= 100; i++)
+            //{
+            //    if(i % 16 == 0)
+            //    {
+            //        Console.WriteLine($"{i}");
+            //    }    
+            //}
+            //Console.ReadKey();
+
+            //задание 5
+
+            for (int i = 0, j = 30; i + j <= 8; i++, j--)
+            {
+                Console.WriteLine(i + j);
+            }
+            Console.ReadKey();  
         }
     }
 }
