b78fb69573c6e0a8a926bdb35db1eb658fbb74f0
Besideuu <roma_yusupov_06@mail.ru>
2022-10-03 20:53:05 +0500

    text edit

---
+++ pz_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int[] newarr = new int[10];
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = r.Next(-10, 10);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                newarr[i] = arr[i] + 2;
+                Console.Write(newarr[i] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
