9ff094df55a4cef47efc8a4360a76b093731babf
BesideUwU <85500424+ONETAP-iwnl@users.noreply.github.com>
2022-10-05 19:19:16 +0500

    Add files via upload

---
+++ pz_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int count = 0;
+            int m = 4;
+            int n = 4;
+            int[,] matrix = new int[m, n];
+            Console.WriteLine("Сгенерированная матрица");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rnd.Next(2, 8);
+                    Console.Write("{0,3} ", matrix[i, j].ToString());
+                    if ((j > i) && (matrix[i, j] >= 0))
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Количество = {0}", count);
+            Console.ReadKey();
+        }
+    }
+}
